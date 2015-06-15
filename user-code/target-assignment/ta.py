0db152ec0617677067ddbf2860c9d1fcf95d744a
Anshul Kanakia <kanakia@colorado.edu>
2015-06-15 16:04:29 -0700

    toy demo works.

--- user-code/target-assignment/ta.py
+++ user-code/target-assignment/ta.py
@@ -7,15 +7,27 @@ Created on Tue Jun 09 16:19:26 2015
 
 from z3 import *
 
-vars = [None for _ in range(2)]
 
-vars[0] = Real('x' + str(0))
-vars[1] = Real('x' + str(1))
+# Constants
+n = 4; # Number of agents
+t = 3;  # Number of targets
 
-#x = Real('x')
-#y = Real('y')
-s = Solver()
-#s.add(x + y > 5, x > 1, y > 1)
-s.add(vars[0] + vars[1] > 5, vars[0] > 1, vars[1] > 1)
-print(s.check())
-print(s.model())
+# Decision variables
+x = [[Int('x(%d,%d)'%(i,j)) for j in range(t)] for i in range(n)]
+
+# Set up model
+sol = Solver()
+
+
+for i in range(n):
+    for j in range(t):
+        sol.add(Or(x[i][j]==0, x[i][j]==1))
+    sol.add(sum(x[i]) <= 1)
+        
+
+
+# Agent constraints
+sol.add(x[0][2]==0, x[1][0]==0, x[2][2]==0, x[3][0]==0)
+
+print(sol.check())
+print(sol.model())
